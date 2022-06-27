978c2098733836b298faf8b59e4aa9ebb29bd3bf
syedsal435 <108167359+syedsal435@users.noreply.github.com>
2022-06-27 16:48:46 +0530

    ADD

---
+++ salo.js
@@ -0,0 +1,16 @@
+function switchOff() {
+  document.getElementById("bulbImage").src =
+    "Zoop1.png";
+  
+  document.getElementById("switchStatus").textContent = "LESS INFORMATION";
+  
+}
+
+function switchOn() {
+  document.getElementById("bulbImage").src =
+    "Zoop.png";
+ 
+  document.getElementById("switchStatus").textContent = "MORE INFORMATION";
+  
+}
+
